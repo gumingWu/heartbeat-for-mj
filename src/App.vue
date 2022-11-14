61b3985ecaa54700aec6ea1b906518ce53d51d6d
gumingWu <15626185054@163.com>
2022-11-14 13:29:24 +0800

    copy

--- src/App.vue
+++ src/App.vue
@@ -2,8 +2,16 @@
 </script>
 
 <template>
-  hi
+  <div id="container"></div>
 </template>
 
 <style scoped>
+.bg {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: #393733;
+}
 </style>
